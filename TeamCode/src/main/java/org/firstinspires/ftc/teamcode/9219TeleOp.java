c914e222f79d9f6bd8710941344ee28444dd7317
Tom Rishworth <thomas_rishworth@students.nksd.net>
2017-11-14 14:24:47 -0500

    Added Strafing Motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/9219TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/9219TeleOp.java
@@ -10,10 +10,11 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name="9219TeleOp", group="9219Code")
 public class 9219TeleOp extends LinearOpMode {
 
-    private DcMotor RB;
-    private DcMotor RF;
-    private DcMotor LF;
-    private DcMotor LB;
+    private DcMotor RB; //1.0
+    private DcMotor RF; //1.1
+    private DcMotor LF; //1.2
+    private DcMotor LB; //1.3
+    private DcMotor CB; //2.0
 
     @Override
     public void runOpMode() {
@@ -24,12 +25,14 @@ public class 9219TeleOp extends LinearOpMode {
         RF = hardwareMap.get(DcMotor.class, "RFMotor");
         LB  = hardwareMap.get(DcMotor.class, "LBMotor");
         RB = hardwareMap.get(DcMotor.class, "RBMotor");
+        CB = hardwareMap.get(DcMotor.class, "CBMotor");
      
         LF.setDirection(DcMotor.Direction.REVERSE);
         RF.setDirection(DcMotor.Direction.FORWARD);
         RB.setDirection(DcMotor.Direction.FORWARD);
         LB.setDirection(DcMotor.Direction.REVERSE);
-        
+        CB.setDirection(DcMotor.Direction.FORWARD);
+
         waitForStart();
        
         while (opModeIsActive()) {
@@ -38,17 +41,18 @@ public class 9219TeleOp extends LinearOpMode {
             double RBpower = (gamepad1.left_stick_y - gamepad1.right_stick_x);
             double LFpower = (gamepad1.left_stick_y + gamepad1.right_stick_x);
             double LBpower = (gamepad1.left_stick_y + gamepad1.right_stick_x);
-            
+            double CBpower = (gamepad1.left_stick_x + gamepad1.right_stick_y);
     
             RFpower = Range.clip(RFpower,-0.75, 0.75);
             RBpower = Range.clip(RBpower,-0.75, 0.75);
             LFpower = Range.clip(LFpower,-0.75, 0.75);
             LBpower = Range.clip(LBpower,-0.75, 0.75);
-            
+            CBpower = Range.clip(CBpower,-0.75, 0.75);
             LF.setPower(LFpower);
             RF.setPower(RFpower);
             LB.setPower(LBpower);
             RB.setPower(RBpower);
+            CB.setPower(CBpower);
             
         }
     }
